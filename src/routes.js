4030e5fc43d24ac4c55ca286d579568f6fcbc0e2
Raul Neto <dev.raulneto@gmail.com>
2020-03-19 21:39:53 +0000

    Normalizar dados

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
-import { useListVals } from 'react-firebase-hooks/database'
+import { useObjectVal } from 'react-firebase-hooks/database'
 
 import firebase from './providers/Firebase'
 import Dashboard from './components/Dashboard'
@@ -13,48 +13,53 @@ import Topbar from "./components/Topbar";
 
 function Routes() {
 	const fb = useContext(firebase)
-	const [snapshots, loading] = useListVals(fb.db.ref('data'))
+	const [snapshots, loading] = useObjectVal(fb.db.ref('data'))
 	
 	const [data, setData] = useState([])
 	// const [estadosObj, setEstadosObj] = useState({})
 	const [qtd, setQtd] = useState(0)
 
 	useEffect(() => {
+		console.log(snapshots);
 		if (snapshots) {
-			console.log(snapshots)
+			console.log(snapshots);
+			updateValues(snapshots);
 		}
 	}, [snapshots])
 	
-	// const updateValues = (data) => {
-	// 	let _qtd = 0
-	// 	let estadosObj = {}
+	const updateValues = (data) => {
+		let _qtd = 0
+		let estadosObj = {}
 
-	// 	let result = data.reduce((res, value) => {
-	// 		const uf = Estados.find(e => e.nome === value.estado).sigla;
-	// 		if (!res[uf]) {
-	// 			res[uf] = {
-	// 				_qtd: 0,
-	// 				estado: uf
-	// 			}
-	// 		}
-	// 		res[uf].qtd += value.qtd;
-	// 		_qtd += value.qtd;
-	// 		return res;
-	// 	}, estadosObj);
+		let result = data.reduce((res, value) => {
+			const uf = Estados.find(e => e.nome === value.estado).sigla;
+			if (!res[uf]) {
+				res[uf] = {
+					qtd: 0,
+					estado: uf
+				}
+				console.log(res);
+			}
+			res[uf].qtd += value.qtd;
+			_qtd += value.qtd;
+			return res;
+		}, {});
 
-	// 	result = Object.keys(result)
-	// 		.map(r => result[r])
-	// 		.sort(function (a, b) {
-	// 			if (a.estado > b.estado) {
-	// 				return 1;
-	// 			} else {
-	// 				return -1;
-	// 			}
-	// 		});
-
-	// 	setData(result)
-	// 	setQtd(_qtd)
-	// }
+		result = Object.keys(result)
+			.map(r => result[r])
+			.sort(function (a, b) {
+				if (a.estado > b.estado) {
+					return 1;
+				} else {
+					return -1;
+				}
+			});
+		
+			console.log(result);
+			console.log(_qtd);
+		setData(result);
+		setQtd(_qtd);
+	}
 
 	return (
 		<Router>
