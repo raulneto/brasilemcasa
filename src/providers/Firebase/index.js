0b56eb823e6e08e7904505b9ecf94f9199f6308a
netoraul <62386942+netoraul@users.noreply.github.com>
2021-05-20 22:06:41 +0100

    Update index.js

--- src/providers/Firebase/index.js
+++ src/providers/Firebase/index.js
@@ -4,16 +4,7 @@ import app from 'firebase/app'
 import 'firebase/database'
 import 'firebase/analytics'
 
-const config = {
-    apiKey: "AIzaSyC33AIPBIxnBQ25bgIDj19226tBpfYkdgY",
-    authDomain: "brasil-em-casa-443b3.firebaseapp.com",
-    databaseURL: "https://brasil-em-casa-443b3.firebaseio.com",
-    projectId: "brasil-em-casa-443b3",
-    storageBucket: "brasil-em-casa-443b3.appspot.com",
-    messagingSenderId: "855150563502",
-    appId: "1:855150563502:web:b3046b15276e69aa149d3f",
-    measurementId: "G-G3QN1CMXET"
-}
+const config = {}
 
 export class Firebase {
     constructor() {
